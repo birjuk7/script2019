c1aba4145e82f77ba3cac1f7a9250bf807c53675
cogmac <cogmac@localhost.localdomain>
2019-05-27 11:29:00 +0530

    to add services while system reboot

---
+++ start_stop@reboot.sh
@@ -0,0 +1,3 @@
+## add commands in these files to start & stop services at the system boot
+vim /etc/rc.local                     ### for ubuntu
+vim /etc/rc.d/rc.local                ### for centos
