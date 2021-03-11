342864c937d19189038b12f21b462588218a9a85
birju kumar <32246823+birjuk7@users.noreply.github.com>
2021-03-11 17:50:16 +0530

    Delete con.sh

--- con.sh
+++
@@ -1,9 +0,0 @@
-gunzip *.gz                                             #unzip all contract files
-mv cd_contract cd_contract.txt                          #rename 
-mv cd_participant cd_participant.txt          
-mv cd_spd_contract cd_spd_contract.txt 
-mv contract contract.txt 
-mv security security.txt 
-mv spd_contract spd_contract.txt
-cp * /root/NebulaFNORMS/                               #copy all files to NebulaFNORMS
-cp * /root/ConvexFNORMS/                               #COPY all files to ConvexFNORMS
