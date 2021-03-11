e6f47a7e6087066b433c5e4954252067e42ce3bb
birju kumar <32246823+birjuk7@users.noreply.github.com>
2021-03-11 17:51:36 +0530

    Delete bod106.sh

--- bod106.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 106 bod
-i=`date +%d%b%y`; 
-find . -mmin -400 -type d -name /home/cogmac/Documents/NSE_DATA"$i"; 
-cd $i; 
-cd contract_files; 
-sshpass -f /path/of/pass_file/.xyzpass rsync -e "ssh -p xxxx"  /home/cogmac/Documents/NSE_DATA/"$i"/contract_files/* root@xx.xx.xx.x:.
-
